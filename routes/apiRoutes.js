780fa1ab0a72ac89bc9db229fa0db7689325bf76
Terry <t3rrygong@gmail.com>
2020-07-01 15:01:54 -0600

    finished apiroutes and htmlroutes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,31 @@
+var db = require("../models");
+
+module.exports = function (app) {
+    app.get("/api/workouts", function (req, res) {
+        db.Workout.find({}).then(function (data) {
+            res.json(data);
+        });
+    });
+
+    app.put("/api/workouts/:id", function (req, res) {
+        db.Workout.findOneAndUpdate(
+            { _id: req.params.id },
+            { $push: { exercises: req.body } },
+            { new: true }
+        ).then(function (data) {
+            res.json(data);
+        });
+    });
+
+    app.post("/api/workouts", function (req, res) {
+        db.Workout.create(req.body).then(function (data) {
+            res.json(data);
+        });
+    });
+
+    app.get("/api/workouts/range", function (req, res) {
+        db.Workout.find({}).then(function (data) {
+            res.json(data);
+        });
+    });
+}
--- server.js
+++ server.js
@@ -6,14 +6,14 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json);
+app.use(express.json());
 
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/workout",
-    { useNewUrlParser: true }
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
 );
 
 app.listen(PORT, function () {
