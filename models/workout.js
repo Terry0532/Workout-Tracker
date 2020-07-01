e72c38e4fd545de649e0b723c405726f99fa02ce
Terry <t3rrygong@gmail.com>
2020-06-30 21:02:31 -0600

    added models folder with indexjs and workoutjs

---
+++ models/workout.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var workoutSchema = new Schema({
+    day: { type: Date, default: Date.now },
+    exercises: [{
+        type: { type: String, required: true },
+        name: { type: String, required: true },
+        duration: { type: Number, required: true },
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number
+    }]
+});
+
+var Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
